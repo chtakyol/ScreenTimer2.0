aa2997e1122c28f15e083d7bb6f6de4714a4c2e1
chtakyol <chtakyol@gmail.com>
2022-08-19 10:06:20 +0300

    Circular duration progress bar added.

--- app/src/main/java/com/oolong/screentimer20/presentation/countdown_screen/CountdownScreen.kt
+++ app/src/main/java/com/oolong/screentimer20/presentation/countdown_screen/CountdownScreen.kt
@@ -2,6 +2,7 @@ package com.oolong.screentimer20.presentation.countdown_screen
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 
@@ -10,4 +11,11 @@ fun CountdownScreen(
     navController: NavController = rememberNavController()
 ) {
     Text(text = "This is Countdown Screen!")
+}
+
+@Composable
+@Preview
+fun PreviewCountdownScreen()
+{
+
 }
