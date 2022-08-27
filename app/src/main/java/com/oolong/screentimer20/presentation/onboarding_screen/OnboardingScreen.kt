9cbfa6882084d8ab10574dd11814c4b14da1d2e8
chtakyol <chtakyol@gmail.com>
2022-08-27 22:27:23 +0300

    On boarding screen and fonts added.

--- app/src/main/java/com/oolong/screentimer20/presentation/onboarding_screen/OnboardingScreen.kt
+++ app/src/main/java/com/oolong/screentimer20/presentation/onboarding_screen/OnboardingScreen.kt
@@ -1,10 +1,13 @@
 package com.oolong.screentimer20.presentation.onboarding_screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -13,29 +16,45 @@ import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.HorizontalPagerIndicator
 import com.google.accompanist.pager.rememberPagerState
 import com.oolong.screentimer20.Screen
+import com.oolong.screentimer20.presentation.onboarding_screen.components.FirstOnBoardingScreen
 import com.oolong.screentimer20.presentation.onboarding_screen.components.NextButton
-import com.oolong.screentimer20.presentation.onboarding_screen.components.PagerScreen
+import com.oolong.screentimer20.presentation.onboarding_screen.components.SecondOnBoardingScreen
 
 @ExperimentalPagerApi
 @Composable
 fun OnboardingScreen(
     navController: NavController = rememberNavController()
 ) {
-    val pages = listOf(
-        OnboardingPage.First,
-        OnboardingPage.Second,
-        OnboardingPage.Third
-    )
     val pagerState = rememberPagerState()
 
-    Column(modifier = Modifier.fillMaxSize()) {
+    val backgroundColors = listOf(
+        Color(0xFF212121),
+        Color(0xFFBB86FC)
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = backgroundColors
+                )
+            )
+    ) {
         HorizontalPager(
             modifier = Modifier.weight(10f),
-            count = 3,
+            count = 2,
             state = pagerState,
             verticalAlignment = Alignment.Top
         ) { position ->
-            PagerScreen(onboardingPage = pages[position])
+            when (position) {
+                0 -> {
+                    FirstOnBoardingScreen()
+                }
+                1 -> {
+                    SecondOnBoardingScreen()
+                }
+            }
         }
         HorizontalPagerIndicator(
             modifier = Modifier
--- app/src/main/java/com/oolong/screentimer20/ui/theme/Type.kt
+++ app/src/main/java/com/oolong/screentimer20/ui/theme/Type.kt
@@ -1,17 +1,34 @@
 package com.oolong.screentimer20.ui.theme
 
 import androidx.compose.material.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.oolong.screentimer20.R
+
+val Alata = FontFamily(
+    Font(R.font.alata_regular)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+        fontFamily = Alata,
+        fontSize = 20.sp,
+        color = Color(0xFFFFFFFF)
+    ),
+    h1 = TextStyle(
+        fontFamily = Alata,
+        fontSize = 54.sp,
+        color = Color(0xFFFFFFFF)
+    ),
+    h2 = TextStyle(
+        fontFamily = Alata,
+        fontSize = 32.sp,
+        color = Color(0xFFFFFFFF)
     )
     /* Other default text styles to override
     button = TextStyle(
